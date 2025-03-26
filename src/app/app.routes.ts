2a6397ac435841e18a9bbc774f16f3381bd2c8fc
emery97 <s10258645@connect.np.edu.sg>
2025-03-26 13:56:53 +0800

    sankey page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {  Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HighchartsDashboardComponent } from './highcharts-dashboard/highcharts-dashboard.component';   
+import { SankeyComponent } from './sankey/sankey.component';
 
 const routes:Routes = [
     {
@@ -18,6 +19,11 @@ const routes:Routes = [
         path:'highcharts-dashboard',
         component: HighchartsDashboardComponent,
         title : 'Highcharts Dashboard Page',
+    },
+    {
+        path:'financial-sankey',
+        component: SankeyComponent,
+        title : 'Financial Sankey Page',
     },
 ];
 
---
+++ src/app/sankey/sankey.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sankey',
+  imports: [],
+  templateUrl: './sankey.component.html',
+  styleUrl: './sankey.component.css'
+})
+export class SankeyComponent {
+
+}
